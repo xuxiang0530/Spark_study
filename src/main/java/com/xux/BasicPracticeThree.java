28663e83ef301924b25491eacfc41696783ce717
xux <tianyaxx@163.com>
2019-01-15 07:41:51 +0800

    20190115

--- src/main/java/com/xux/BasicPracticeThree.java
+++ src/main/java/com/xux/BasicPracticeThree.java
@@ -3,11 +3,13 @@ package com.xux;
 
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import scala.Tuple2;
 
 import java.io.Serializable;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 public class BasicPracticeThree {
@@ -30,12 +32,26 @@ public class BasicPracticeThree {
 
         // TODO add your code here
 //        计算相同Key对应的的所有value的平均值，并输出到目录/tmp/output下
+        JavaPairRDD<String,SumAndCount> sumAndCouuntRdd = input.groupByKey()
+                .map(x -> {
+                    Iterator<Integer> iv =  x._2.iterator();
+                    Integer sum = 0;
+                    Integer count = 0;
+                    while (iv.hasNext())
+                    {
+                        sum += iv.next();
+                        count++;
+                    }
+                    return new Tuple2( x._1, new SumAndCount(sum,count));
+                });
+
         jsc.stop();
     }
 }
 class SumAndCount implements Serializable {
     final int sum;
     final int count;
+
     public SumAndCount(int sum, int count) {
         this.sum = sum;
         this.count = count;
